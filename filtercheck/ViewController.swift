755e6b2af08af4b8d1c0baa81ebece7aec77742a
土屋絢大 <kenta@S-Tsuchiya-Kenta.local>
2017-07-08 19:15:33 +0900

    Initial Commit

---
+++ filtercheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  filtercheck
+//
+//  Created by 土屋絢大 on 2017/07/08.
+//  Copyright © 2017年 Mr.TUTTY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
